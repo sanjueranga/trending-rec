9459764d090673bcc1463afd576bfe85c03580b7
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2025-09-27 10:07:13 +0530

    wip

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api',
+  },
 };
 
 export default nextConfig;
